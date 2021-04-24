996e296792ef7604ea8a2481b6c42b6755b56f13
TakashiAihara <takashi.aihara.0523@gmail.com>
2021-04-24 12:17:02 +0900

    first commit

---
+++ t2j.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- Coding: utf-8 -*-
+import sys
+import json
+
+lines = sys.stdin.readlines()
+ret=[]
+keys=[]
+for i,l in enumerate(lines):
+    if i==0:
+        keys=[_i.strip() for _i in l.split('|')]
+    elif i==1: continue
+    else:
+        ret.append({keys[_i]:v.strip() for _i,v in enumerate(l.split('|')) if  _i>0 and _i<len(keys)-1})
+print(json.dumps(ret, indent = 4))
